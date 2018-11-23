3bbf87bd5aa8117c95f22f3e6f48fc83ac292e98
kyaw <kss.kyawsiesein@gmail.com>
2018-11-23 13:05:27 +1100

    Added formik

--- src/Home.js
+++ src/Home.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import {Subscribe} from 'unstated'
 import TaskContainer from './TaskContainer'
+import {Formik, ErrorMessage} from 'formik'
 
 class Home extends React.Component {
-  state = {
-    taskValue: '',
-  }
-
-  onChange = e => {
-    this.setState({taskValue: e.target.value})
-  }
-
   addTaskCallback = () => {
     this.props.history.push('/results')
+  }
+
+  formValidation = (values, props) => {
+    let errors = {}
+    if (values.task.trim().length === 0) {
+      errors.task = 'Required'
+    }
+
+    return errors
+  }
+
+  onSubmit = (values, args, next) => {
+    next(values.task, this.addTaskCallback)
   }
 
   render() {
@@ -20,12 +26,23 @@ class Home extends React.Component {
       <Subscribe to={[TaskContainer]}>
         {tasks => {
           return (
-            <div>
-              <input value={this.state.taskValue} onChange={this.onChange} />
-              <button onClick={() => tasks.addTasks(this.state.taskValue, this.addTaskCallback)}>
-                Add task
-              </button>
-            </div>
+            <Formik
+              initialValues={{task: ''}}
+              validate={this.formValidation}
+              onSubmit={(values, args) => this.onSubmit(values, args, tasks.addTasks)}
+            >
+              {formValues => {
+                return (
+                  <form onSubmit={formValues.handleSubmit}>
+                    <input value={formValues.task} name="task" onChange={formValues.handleChange} />
+                    <span className="error">
+                      <ErrorMessage name="task" />
+                    </span>
+                    <button type="submit">Add task</button>
+                  </form>
+                )
+              }}
+            </Formik>
           )
         }}
       </Subscribe>
